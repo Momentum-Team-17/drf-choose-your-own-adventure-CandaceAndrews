bf972cc579f72a18704f9334d73d512c9fcf91c9
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-03-21 21:44:52 -0400

    edited date to just year, order by title for books

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Book, Author
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Author)
--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import UniqueConstraint
 
 
 class User(AbstractUser):
@@ -14,19 +15,42 @@ class Book(models.Model):
         ('romance', 'Romance'),
         ('fantasy', 'Fantasy'),
         ('thriller', 'Thriller'),
+        ('mystery', 'Mystery'),
+        ("children's literature", "Children's Literature"),
+        ('biography', 'Biograpy'),
+        ('adventure', 'Adventure'),
+        ('cookbook', 'Cookbook'),
+        ('historical', 'Historical'),
+        ('fiction', 'Fiction'),
+        ('nonfiction', 'Non-fiction'),
+        ('science fiction', 'Science Fiction'),
     )
 
     title = models.CharField(max_length=100)
     author = models.ForeignKey(
-        to='Author', on_delete=models.CASCADE, related_name='author')
-    date_published = models.DateField(blank=True, null=True)
+        to='Author',
+        on_delete=models.CASCADE,
+    )
+    year_published = models.IntegerField(blank=True, null=True)
     genre = models.CharField(choices=CHOICES, max_length=50)
-    # featured =
+    featured = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['author', 'title'],
+                             name='unique_constraint')
+        ]
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
 
 # class Notes(models.Model):
 #     pass
